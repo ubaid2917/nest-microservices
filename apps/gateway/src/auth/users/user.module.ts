59eba38affd0c2e57e52064596c2db402a035ffa
ubaid2917 <ubaid29170@gmail.com>
2026-01-07 23:38:59 +0500

    register user model in module

---
+++ apps/gateway/src/auth/users/user.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { InjectModel, MongooseModule } from '@nestjs/mongoose';
+import { User, UserDocument } from './schema/user.schema';
+import { Model } from 'mongoose';
+
+@Module({
+  imports: [
+   MongooseModule.forFeature([{ name: User.name, schema: UserDocument }])
+  ],
+  controllers: [],
+  providers: [UserService],
+  exports: [],
+})
+export class UsersModule {}
