a67bb1d7c5b95afa31b719d60c359261480704a0
ubaid2917 <ubaid29170@gmail.com>
2026-01-07 23:09:46 +0500

    add user serice and schema

---
+++ apps/gateway/src/auth/users/schema/user.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import {UserType} from '../../enums/user-type.enum'
+import {HydratedDocument} from "mongoose" 
+
+export type UserDocument = HydratedDocument<User>;
+@Schema({timestamps: true})
+export class User {
+    @Prop({required: true, unique: true, index: true})
+    clerkUserId: string;
+
+    @Prop({required: true})
+    email: string;
+
+    @Prop({required: true})
+    name: string;
+
+    
+    @Prop({required: true, enum: UserType, default: UserType.USER})
+    role: UserType; 
+
+    @Prop({required: true, default: false})
+    isAdmin: boolean;
+
+    @Prop({})
+    password: string;
+}
+
+export const UserDocument = SchemaFactory.createForClass(User);
