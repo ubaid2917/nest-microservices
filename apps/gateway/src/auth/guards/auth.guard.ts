44ae0b5095de2617ac7c6106fe658b71fdd6dd0a
ubaid3720 <ubaid@syncscrum.com>
2026-01-08 19:15:53 +0500

    fixed job list issue

--- apps/gateway/src/auth/guards/auth.guard.ts
+++ apps/gateway/src/auth/guards/auth.guard.ts
@@ -1,11 +1,70 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthService } from '../auth.service';
+import { UserService } from '../users/user.service';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { REQUIRED_ROLE_KEY } from '../decorators/admin.decorator';
+import { UserType } from '../enums/user-type.enum';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    //  Allow public routes
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) return true;
+
+    const request = context.switchToHttp().getRequest();
+
+    //  Authorization header validation
+    const authorization = request.headers['authorization'];
+
+    if (!authorization) {
+      throw new UnauthorizedException('Authorization header is required');
+    }
+
+    const [type, token] = authorization.split(' ');
+
+    if (type !== 'Bearer' || !token) {
+      throw new UnauthorizedException('Invalid authorization header format');
+    }
+
+    //  Verify token & build auth context
+    const verifiedUser = await this.authService.verifyAndBuildContext(token);
+
+    //  Upsert user in DB
+    const dbUser = await this.userService.upsertAuthUser(verifiedUser);
+
+    //  Remove sensitive fields
+    const { password, ...user } = dbUser;
+
+    request.user = user;
+
+    // Role-based access control
+    const requiredRole = this.reflector.getAllAndOverride<UserType>(
+      REQUIRED_ROLE_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (requiredRole === UserType.ADMIN && user.role !== UserType.ADMIN) {
+      throw new ForbiddenException('Admin access required');
+    }
+
     return true;
   }
 }
--- apps/gateway/src/auth/users/user.service.ts
+++ apps/gateway/src/auth/users/user.service.ts
@@ -1,25 +1,32 @@
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose"; 
-import {User, UserDocument} from './schema/user.schema'
-import { Model } from "mongoose";
-import { CreateUserDto } from "./dtos/create-user.dto";
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schema/user.schema';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserType } from '../enums/user-type.enum';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectModel(User.name)
-        private readonly userModel : Model<UserDocument>
-    ){}
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>,
+  ) {}
 
-    async upsertAuthUser(user: CreateUserDto){
-        return this.userModel.findOneAndUpdate(
-            {clerkUserId: user.clerkUserId},
-            user,
-            {upsert: true, new: true}
-        )
-    } 
+  async upsertAuthUser(user: CreateUserDto) {
+    return this.userModel.findOneAndUpdate(
+      { clerkUserId: user.clerkUserId },
+      user,
+      {
+        upsert: true,
+        new: true,
+        $setOnInsert: {
+          role: UserType.USER,
+        },
+      },
+    );
+  }
 
-    async findByClerkUserId(clerkUserId: string){
-        return this.userModel.findOne({clerkUserId})
-    }
+  async findByClerkUserId(clerkUserId: string) {
+    return this.userModel.findOne({ clerkUserId });
+  }
 }
